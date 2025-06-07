85c51abd9bc00fe0e290127a6802773923c51ee8
Adam <adam.conkle@gmail.com>
2025-06-06 22:50:45 -0700

    added iris and gaze

--- script.js
+++ script.js
@@ -17,21 +17,54 @@ faceMesh.onResults(results => {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
+  let gazeDirection = '';
+
   if (results.multiFaceLandmarks.length > 0) {
-    for (const landmarks of results.multiFaceLandmarks) {
-      for (let i of [33, 133, 362, 263]) { // Key points around eyes
-        const x = landmarks[i].x * canvasElement.width;
-        const y = landmarks[i].y * canvasElement.height;
-        canvasCtx.beginPath();
-        canvasCtx.arc(x, y, 4, 0, 2 * Math.PI);
-        canvasCtx.fillStyle = 'cyan';
-        canvasCtx.fill();
-      }
-    }
+    const landmarks = results.multiFaceLandmarks[0];
+
+    // --- Left Eye ---
+    const leftEyeLeft = landmarks[33];
+    const leftEyeRight = landmarks[133];
+    const leftIris = landmarks[468];
+
+    const leftEyeWidth = leftEyeRight.x - leftEyeLeft.x;
+    const leftIrisOffset = (leftIris.x - leftEyeLeft.x) / leftEyeWidth;
+
+    // --- Right Eye ---
+    const rightEyeLeft = landmarks[362];
+    const rightEyeRight = landmarks[263];
+    const rightIris = landmarks[473];
+
+    const rightEyeWidth = rightEyeRight.x - rightEyeLeft.x;
+    const rightIrisOffset = (rightIris.x - rightEyeLeft.x) / rightEyeWidth;
+
+    // Average both eyes
+    const avgIrisOffset = (leftIrisOffset + rightIrisOffset) / 2;
+
+    // --- Determine direction ---
+    if (avgIrisOffset < 0.35) gazeDirection = 'Looking Left';
+    else if (avgIrisOffset > 0.65) gazeDirection = 'Looking Right';
+    else gazeDirection = 'Looking Center';
+
+    // Draw key points
+    [33, 133, 362, 263, 468, 473].forEach(i => {
+      const x = landmarks[i].x * canvasElement.width;
+      const y = landmarks[i].y * canvasElement.height;
+      canvasCtx.beginPath();
+      canvasCtx.arc(x, y, 4, 0, 2 * Math.PI);
+      canvasCtx.fillStyle = 'cyan';
+      canvasCtx.fill();
+    });
+
+    // Draw gaze direction text
+    canvasCtx.font = '24px Arial';
+    canvasCtx.fillStyle = 'lime';
+    canvasCtx.fillText(gazeDirection, 20, 30);
   }
 
   canvasCtx.restore();
 });
+
 
 // Setup webcam stream
 const camera = new Camera(videoElement, {
