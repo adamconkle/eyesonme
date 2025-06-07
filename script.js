61ed641a0edb92402684564f3074b604cdcb16e2
Adam <adam.conkle@gmail.com>
2025-06-06 23:08:02 -0700

    re full update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const videoElement = document.getElementById('video');
 const canvasElement = document.getElementById('overlay');
 const canvasCtx = canvasElement.getContext('2d');
 
-// Setup MediaPipe FaceMesh
+// Setup FaceMesh
 const faceMesh = new FaceMesh({
   locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
 });
@@ -13,46 +13,48 @@ faceMesh.setOptions({
   minTrackingConfidence: 0.5
 });
 
+// Handle FaceMesh results
 faceMesh.onResults(results => {
   canvasCtx.save();
-  canvasCtx.scale(-1, 1); // Flip canvas to match flipped video
-  canvasCtx.translate(-canvasElement.width, 0);
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
-  let gazeX = '';
-  let gazeY = '';
+  // Mirror canvas to match mirrored video
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.scale(-1, 1);
+  canvasCtx.translate(-canvasElement.width, 0);
+
+  let gazeX = '', gazeY = '';
 
   if (results.multiFaceLandmarks.length > 0) {
     const landmarks = results.multiFaceLandmarks[0];
 
-    // --- Left Eye ---
+    // Left Eye
+    const leftIris = landmarks[468];
     const leftEyeLeft = landmarks[33];
     const leftEyeRight = landmarks[133];
     const leftEyeTop = landmarks[159];
     const leftEyeBottom = landmarks[145];
-    const leftIris = landmarks[468];
 
     const leftWidth = leftEyeRight.x - leftEyeLeft.x;
     const leftHeight = leftEyeBottom.y - leftEyeTop.y;
-    const leftIrisXOffset = (leftIris.x - leftEyeLeft.x) / leftWidth;
-    const leftIrisYOffset = (leftIris.y - leftEyeTop.y) / leftHeight;
+    const leftXRatio = (leftIris.x - leftEyeLeft.x) / leftWidth;
+    const leftYRatio = (leftIris.y - leftEyeTop.y) / leftHeight;
 
-    // --- Right Eye ---
+    // Right Eye
+    const rightIris = landmarks[473];
     const rightEyeLeft = landmarks[362];
     const rightEyeRight = landmarks[263];
     const rightEyeTop = landmarks[386];
     const rightEyeBottom = landmarks[374];
-    const rightIris = landmarks[473];
 
     const rightWidth = rightEyeRight.x - rightEyeLeft.x;
     const rightHeight = rightEyeBottom.y - rightEyeTop.y;
-    const rightIrisXOffset = (rightIris.x - rightEyeLeft.x) / rightWidth;
-    const rightIrisYOffset = (rightIris.y - rightEyeTop.y) / rightHeight;
+    const rightXRatio = (rightIris.x - rightEyeLeft.x) / rightWidth;
+    const rightYRatio = (rightIris.y - rightEyeTop.y) / rightHeight;
 
-    const avgX = (leftIrisXOffset + rightIrisXOffset) / 2;
-    const avgY = (leftIrisYOffset + rightIrisYOffset) / 2;
+    const avgX = (leftXRatio + rightXRatio) / 2;
+    const avgY = (leftYRatio + rightYRatio) / 2;
 
-    // Gaze detection
+    // Gaze direction logic
     if (avgX < 0.35) gazeX = 'Left';
     else if (avgX > 0.65) gazeX = 'Right';
     else gazeX = 'Center';
@@ -61,7 +63,7 @@ faceMesh.onResults(results => {
     else if (avgY > 0.65) gazeY = 'Down';
     else gazeY = 'Center';
 
-    // Draw key points
+    // Draw eye/iris landmarks
     [33, 133, 362, 263, 468, 473, 159, 145, 386, 374].forEach(i => {
       const x = landmarks[i].x * canvasElement.width;
       const y = landmarks[i].y * canvasElement.height;
@@ -71,15 +73,18 @@ faceMesh.onResults(results => {
       canvasCtx.fill();
     });
 
+    canvasCtx.restore(); // Unflip for drawing text
+
+    // Draw normal (non-mirrored) text
     canvasCtx.font = '24px Arial';
     canvasCtx.fillStyle = 'lime';
     canvasCtx.fillText(`Gaze: ${gazeX} / ${gazeY}`, 20, 30);
+  } else {
+    canvasCtx.restore();
   }
-
-  canvasCtx.restore();
 });
 
-// Webcam + camera utility
+// Start webcam
 const camera = new Camera(videoElement, {
   onFrame: async () => {
     await faceMesh.send({ image: videoElement });
@@ -89,7 +94,7 @@ const camera = new Camera(videoElement, {
 });
 camera.start();
 
-// Sync canvas size with video
+// Sync canvas to video size
 videoElement.addEventListener('loadeddata', () => {
   canvasElement.width = videoElement.videoWidth;
   canvasElement.height = videoElement.videoHeight;
